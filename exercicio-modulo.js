2b5d3e05ff4b50d268d82d6180a90b42631f17c5
Matheus Garajau <matheusgarajaucontato@gmail.com>
2026-04-14 09:41:46 -0300

    corrigindo o exercicio do modulo

--- exercicio-modulo.js
+++ exercicio-modulo.js
@@ -1,33 +1,37 @@
 const tarefasQuarto = [
    {descrição: 'Dobrar lençol',
-    prioridade: '1', 
+    prioridade: 1, 
     status: 'pendente'},
 
    {descrição: 'Varrer o chão', 
-    prioridade: '2', 
+    prioridade: 2, 
     status: 'pendente'},
 ];
 
-tarefasQuarto.push({descrição: 'Tirar poeira', 
-                    prioridade: '3', 
-                    status: 'pendente'});
+const tarefasAtualizadas = [
+...tarefasQuarto,
+{
+descricao: 'Tirar poeira',
+prioridade: 3,
+status: 'pendente'
+}
+];
 
-const tarefasPendentes = tarefasQuarto.filter(({ status }) => status === 'pendente');
+const tarefasPendentes = tarefasAtualizadas.filter(({ status }) => status === 'pendente');
 
 const tarefasConcluidas = tarefasPendentes.map(tarefa => ({ ...tarefa, status: 'concluído' }));
 
-// Corrigido o erro de digitação e adicionado o spread [...] para não mutar o original
-const prioridadeTarefas = [...tarefasQuarto].sort((prevTarefa, currentTarefa) => 
+const prioridadeTarefas = [...tarefasAtualizadas].sort((prevTarefa, currentTarefa) => 
   prevTarefa.prioridade - currentTarefa.prioridade
 );
 
-const contarTarefasPendentes = tarefasQuarto.reduce((prev, next) => 
+const contarTarefasPendentes = tarefasAtualizadas.reduce((prev, next) => 
   next.status === 'pendente' ? prev + 1 : prev, 0
 );
 
-const tarefaSelecionada = tarefasQuarto.find(({ descrição }) => descrição === 'Varrer o chão');
+const tarefaSelecionada = tarefasAtualizadas.find(({ descricao }) => descricao === 'Varrer o chão');
 
-const todasTarefasConcluidas = tarefasQuarto.every(({ status }) => status === 'concluído');
+const todasTarefasConcluidas = tarefasAtualizadas.every(({ status }) => status === 'concluído');
 
 
 
